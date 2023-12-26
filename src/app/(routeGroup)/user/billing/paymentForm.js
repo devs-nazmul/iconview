48a1d71b2c2bf0974b13f117ab782a077049dca1
Nazmul Hossain <Belivup@gmail.com>
2023-12-26 21:44:01 +0600

    wehbook fix

--- src/app/(routeGroup)/user/billing/paymentForm.js
+++ src/app/(routeGroup)/user/billing/paymentForm.js
@@ -16,7 +16,7 @@ export default function PaymentForm({items}) {
 	
 	useEffect( () => {
 		
-		fetch(`${process.env.NEXT_PUBLIC_SSL}${process.env.NEXT_PUBLIC_URL}/api/payinit`, {
+		fetch("/api/payinit", {
 			method: "POST",
 			headers: { "Content-Type": "application/json" },
 			body: JSON.stringify({
