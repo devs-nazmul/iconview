8afc30b460facd79d0d6a2a056eaacbab73c63ae
Nazmul Hossain <Belivup@gmail.com>
2024-05-14 17:21:54 +0600

    update content

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,8 +16,19 @@ const poppins = Poppins({
 })
 
 export const metadata = {
-  title: 'iconview.org',
-  description: 'Open Source icon Library, Get Access to 100k+ Svg High Quality Vector Icons for React, Nextjs, React Native and Figma. Unlimited Download For Free.',
+    title: "Open Source icon library. 100k+ icons for React, Nextjs | www.iconview.org",
+    description: "Open Source icon Library, Get Access to 100k+ Svg High Quality Vector Icons for React, Nextjs, React Native and Figma. Unlimited Download For Free.",
+    keywords: ["iconview", "Open Source icon library", "React icon Library", "Nextjs icon Library", "Icons", "dev.nazmul.co", "Free SVG icon", "free icon" ],
+    authors: {name: "Nazmul Hossain", url: "https://dev.nazmul.co/" },
+    metadataBase: new URL('https://iconview.org'),
+    alternates: {
+        canonical: '/',
+        languages: {
+            'en-US': '/en-US'},
+    },
+    openGraph: {
+        images: '/og_image.png',
+    },
 }
 
 export default async function RootLayout({ children }) {
